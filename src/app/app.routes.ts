2bf189360120377d918bf9c5f48351988d9f69c7
Dhouha <dhouha.mansour11@gmail.com>
2026-03-03 14:12:30 +0100

    first application structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'customers' },
+  {
+    path: 'customers',
+    loadComponent: () =>
+      import('./features/customers/customer-list/customer-list').then((m) => m.CustomerList),
+  },
+  {
+    path: 'subscriptions',
+    loadComponent: () =>
+      import('./features/subscriptions/subscription-list/subscription-list').then(
+        (m) => m.SubscriptionList,
+      ),
+  },
+  {
+    path: 'invoices',
+    loadComponent: () =>
+      import('./features/invoices/invoice-list/invoice-list').then((m) => m.InvoiceList),
+  },
+];
