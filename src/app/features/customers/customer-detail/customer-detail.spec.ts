c5ebfc31fa9ce42b82a035f2ed2be4074842a41d
Dhouha <dhouha.mansour11@gmail.com>
2026-03-05 13:23:04 +0100

    fix failing unit test

--- src/app/features/customers/customer-detail/customer-detail.spec.ts
+++ src/app/features/customers/customer-detail/customer-detail.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CustomerDetail } from './customer-detail';
+import { provideRouter } from '@angular/router';
 
 describe('CustomerDetail', () => {
   let component: CustomerDetail;
@@ -9,6 +10,7 @@ describe('CustomerDetail', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [CustomerDetail],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(CustomerDetail);
--- src/app/shared/pages/not-found/not-found.spec.ts
+++ src/app/shared/pages/not-found/not-found.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { NotFound } from './not-found';
-import { provideRouter } from '@angular/router';
 
 describe('NotFound', () => {
   let component: NotFound;
